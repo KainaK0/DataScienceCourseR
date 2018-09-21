3177067e7e5b9d9e0faeae8beca1bdbd8e43c421
Cesar Abilio Flores Molina <ConsultSAP07@cmbsaa.com>
2018-09-21 18:06:54 -0500

    finifhint the chapter

--- MeScripts/tema01/13_dataCorrection.R
+++ MeScripts/tema01/13_dataCorrection.R
@@ -1,3 +1,4 @@
+setwd(dir = "GitProjects/DataScienceCourseR/MeScripts/tema01/")
 install.packages("tidyr")
 library(tidyr)
 
@@ -5,3 +6,36 @@ crime.data <- read.csv("../../r-course/data/tema1/USArrests.csv", stringsAsFacto
 View(crime.data)
 
 crime.data <- cbind(state = rownames(crime.data), crime.data)
+crime.data.1 <- gather(crime.data,
+                       key = "crime_type",
+                       value = "arrest_estimate",
+                       Murder : UrbanPop)
+View(crime.data.1)
+
+crime.data.2 <- gather(crime.data,
+                     key = "crime_type",
+                     value = "arrest_estimate",
+                     -state)
+View(crime.data.2)
+
+crime.data.3 <- gather(crime.data,
+                       key="crime_type",
+                       value = "arrest_estimate",
+                       Murder, Assault)
+View(crime.data.3)
+
+crime.data.4 <- spread(crime.data.2,
+                       key = "crime_type",
+                       value = "arrest_estimate")
+View(crime.data.4)
+
+crime.data.5 <- unite(crime.data,
+                      col = "Murder_Assault",
+                      Murder, Assault, sep = "_")
+View(crime.data.5)
+
+crime.data.6 <- separate(crime.data.5,
+                         col = "Murder_Assault",
+                         into = c("Murder", "Assault"),
+                         sep = "_")
+View(crime.data.6)

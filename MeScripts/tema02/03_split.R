dab0cb2ac1afb7de111c4c64af57cb1d89ec80e1
KainaK0 <johancb1411@gmail.com>
2018-09-25 10:16:18 -0500

    adding script 03 from tema 02

---
+++ MeScripts/tema02/03_split.R
@@ -0,0 +1,16 @@
+#split / unsplit
+
+data <- read.csv("../../r-course/data/tema2/auto-mpg.csv", stringsAsFactors = F)
+
+View(data)
+
+carsplit <- split(data, data$cylinders)
+
+carsplit[1] #get the object '3' 
+carsplit[[1]] #get the dataframe got on '3'
+
+str(carsplit[1])
+str(carsplit[[1]])
+
+names(carsplit[1])
+names(carsplit[[1]])
